9ad277c2ded19a26f6ccbb3a2600b37d85031693
DrobotunS <104461332+DrobotunS@users.noreply.github.com>
2022-12-10 15:25:33 +0200

    fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,40 +1,47 @@
 import PropTypes from 'prop-types';
-import css from './Statistics.module.css' 
+import css from './Statistics.module.css';
 
-function Statistics ({ good, neutral, bad, total, positivePercentage }) {
-    
-        return (
-            <div>
-            
+function Statistics({ good, neutral, bad, total, positivePercentage }) {
+  return (
+    <div>
       <h3 className={css.title}>Statistics</h3>
       <ul>
         <li className={css.statisticsList}>
-            <p>Good: <span>{good}</span></p>
+          <p>
+            Good: <span>{good}</span>
+          </p>
         </li>
         <li className={css.statisticsList}>
-        <p>Neutral: <span>{neutral}</span></p>
+          <p>
+            Neutral: <span>{neutral}</span>
+          </p>
         </li>
         <li className={css.statisticsList}>
-        <p>Bad: <span>{bad}</span></p>
+          <p>
+            Bad: <span>{bad}</span>
+          </p>
         </li>
         <li className={css.statisticsList}>
-        <p>Total: <span>{total}</span></p>
+          <p>
+            Total: <span>{total}</span>
+          </p>
         </li>
         <li className={css.statisticsList}>
-        <p>Positive feedback: <span>{positivePercentage}%</span></p>
+          <p>
+            Positive feedback: <span>{positivePercentage}%</span>
+          </p>
         </li>
       </ul>
-      </div>
-        );
-    }
+    </div>
+  );
+}
 
+Statistics.prototype = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.string.isRequired,
+};
 
-    Statistics.prototype = {
-      good: PropTypes.number.isRequired,
-      neutral: PropTypes.number.isRequired,
-      bad: PropTypes.number.isRequired,
-      total: PropTypes.number.isRequired,
-      positivePercentage: PropTypes.string.isRequired,
-    };
-    
-  export default Statistics;
+export default Statistics;
